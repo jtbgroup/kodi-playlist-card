2c2e2562f5549adb837ff01fc97c4294f0d9e00d
Gautier Vanderslyen <gautier.vanderslyen@gmail.com>
2021-01-10 21:21:30 +0100

    layout nearly done and control buttons added

---
+++ jsonrpc examples.md
@@ -0,0 +1,132 @@
+# JSON RPC examples for Kodi
+
+Home assistant event listener:**kodi_call_method_result**
+
+
+## Get albums
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc": "2.0", "method": "JSONRPC.Introspect", "params": {"filter": {"id": "AudioLibrary.GetAlbums", "type": "method"}}, "id": 1}
+
+### HA service call
+entity_id: media_player.kodi
+method: JSONRPC.Introspect
+item: {"filter":{"id":"AudioLibrary.GetAlbums", "type": "method"}}
+
+### HA service call
+entity_id: media_player.kodi
+method: JSONRPC.Introspect
+filter:
+  id: "AudioLibrary.GetAlbums"
+  type: "method"
+
+## Application get properties
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc": "2.0", "method": "Application.GetProperties", "params": {"properties": ["volume"]}, "id": 1}
+
+### HA service call
+entity_id: media_player.kodi
+method: Application.GetProperties
+item: {"properties":["volume"]}
+
+### HA service call
+entity_id: media_player.kodi
+method: Application.GetProperties
+properties:
+  - volume
+
+## Get Playlist items
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Playlist.GetItems","id":"1609497939295","params":{"playlistid":0,"properties":["title","thumbnail","file","artist","genre","year","rating","album","track","duration","playcount","dateadded","episode","artistid","albumid","tvshowid"],"limits":{"start":0}}}
+
+{"playlistid":0,"properties":["title","thumbnail","file","artist","genre","year","rating","album","track","duration","playcount","dateadded","episode","artistid","albumid","tvshowid"],"limits":{"start":0}}
+
+### HA service call
+entity_id: media_player.kodi
+method: Playlist.GetItems
+item: {"playlistid":0,"properties":["title","thumbnail","file","artist","genre","year","rating","album","track","duration","playcount","dateadded","episode","artistid","albumid","tvshowid"],"limits":{"start":0}}
+
+### HA service call
+entity_id: media_player.kodi
+method: Playlist.GetItems
+playlistid: 0
+properties:
+  - title
+limits:
+  start: 0
+
+## Open a playlist in random mode
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Player.Open","id":"1609497939295","params":{"partymode": "special://profile/playlists/music/Playlist Quiet.xsp"}}
+
+{"partymode": "special://profile/playlists/music/Playlist Quiet.xsp"}
+
+### HA service call
+entity_id: media_player.kodi
+method: Player.Open
+item: {"partymode": "special://profile/playlists/music/Playlist Quiet.xsp"}
+
+### HA service call
+//not ok
+entity_id: media_player.kodi
+method: Player.Open
+item:
+	- partymode: "special://profile/playlists/music/Playlist Quiet.xsp"
+
+## Next item in the playlist
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Player.GoTo","id":"1609497939295","params":{"playerid": 0, "to": "next"}}
+
+### HA service call
+entity_id: media_player.raspberrypi
+method: Player.GoTo
+item: {"playerid": 0, "to": "next"}
+
+### HA service call
+entity_id: media_player.raspberrypi
+method: Player.GoTo
+playerid: 0
+to: "next"
+
+### HA service call
+entity_id: media_player.raspberrypi
+method: Player.GoTo
+playerid: 0
+to: 2 // position is 0 based
+
+## Remove item in the playlist
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Player.Remove","id":"1609497939295","params":{"playerid": 0, "positionid": 3}}
+
+### HA service call
+entity_id: media_player.raspberrypi
+method: Player.GoTo
+playerid: 0
+to: 2 // position is 0 based
+
+## Get Players
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Player.GetPlayers","id":"1609497939295"} 
+
+### HA service call
+//not ok?????
+entity_id: media_player.raspberrypi
+method: Player.GetPlayers
+
+## Player pause
+### http call
+http://192.168.1.12:8080/jsonrpc?request={"jsonrpc":"2.0","method":"Player.PlayPause", "params":{playerid":"1"}} 
+
+### HA service call
+entity_id: media_player.raspberrypi
+method: Player.PlayPause
+playerid: 0
+
+## Recent added videos
+### HA service call
+entity_id: media_player.raspberrypi
+method: VideoLibrary.GetRecentlyAddedMovies
+properties: 
+	- title
+	- year
+	- file
+	- lastplayed
--- playlist-media-card.js
+++ playlist-media-card.js
@@ -1,122 +1,224 @@
 class PlaylistMediaCard extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
-      const card = document.createElement("ha-card");
-      card.header = this.config.title;
+      this._hass = hass;
+      this.card = document.createElement("ha-card");
+      this.card.header = this.config.title;
       this.content = document.createElement("div");
       this.content.style.padding = "5px 10px";
-      card.appendChild(this.content);
-      this.appendChild(card);
+      this.card.appendChild(this.content);
+      this.appendChild(this.card);
     }
 
     let style = document.createElement("style");
-    style.setAttribute("id", "umc_style");
-
     style.textContent = `
-                li {
-                  border: 1px solid #ccc;
-                }
 
-                .playlist-items{
-                  list-style-type: none
+                // .thumbnailCell, .titleCell, .genreCell, .albumCell, .durationCell, .removeCell{
+                //   border: 1px solid orange;
+                // }
+                .playerType{
+                  width: 100%;
+                  height: 30px;
+                  text-align: right;
+                  font-weight: bold;
+                  font-size: 18px;
                 }
 
                 .inner-item{
                   display: grid;
-                  grid-template-columns: 55px auto 50px;
-                  grid-gap: 5px;
+                  grid-template-columns: 65px 1fr auto auto;
+                  grid-gap: 4px;
                   grid-auto-rows: auto;
                 }
-                .thumb_img{
-                  width: 50px;
-                  height: 50px;
-                  display: block;
-                  background-size: cover;
-                }
 
-                .thumbnail {
+                .thumbnailCell{
                   grid-column-start: 1;
                   grid-column-end: 2;
                   grid-row-start: 1;
                   grid-row-end: 4;
-                  border: 1px solid orange;
+                  display: block;
+                  background-size: contain;
+                  background-repeat: no-repeat;
+                  width: 65px;
+                  height: 65px;
                 }
 
-                .title{
-                  border: 1px solid red;
+                .thumbnailPlayCell{
+                  grid-column-start: 1;
+                  grid-column-end: 2;
+                  grid-row-start: 1;
+                  grid-row-end: 4;
+                  background-color: rgba(255, 255, 255, .4);
+                  display: block;
+                  background-size: cover;
+                  width: 65px;
+                  height: 65px;
+                }
+
+                .titleCell{
                   grid-column-start: 2;
-                  grid-column-end: end;
+                  grid-column-end: end-1;
                   grid-row-start: 1;
                   grid-row-end: 2;
+                  font-weight: bold;
+                  font-size: 14px;
+                  background: rgb(230,230,230);
+                  background: linear-gradient(90deg, rgba(230,230,230,1) 10%, rgba(255,255,255,1) 90%);
                 }
-
-                .album{
-                  border: 1px solid green;
+                .genreCell{
                   grid-column-start: 2;
                   grid-column-end: 3;
                   grid-row-start: 2;
                   grid-row-end: 3;
+                  font-style: italic;
                 }
-                .genre{
-                  border: 1px solid yellow;
+                .albumCell{
                   grid-column-start: 2;
                   grid-column-end: 3;
                   grid-row-start: 3;
                   grid-row-end: 4;
                 }
-                .duration{
-                  border: 1px solid black;
+                .durationCell{
                   grid-column-start: 3;
                   grid-column-end: end;
-                  grid-row-start: 2;
-                  grid-row-end: 3;
+                  grid-row-start: 3;
+                  grid-row-end: 4;
                   text-align: right;
+                }
+
+                .removeCell{
+                  grid-column-start: 4;
+                  grid-column-end: end;
+                  grid-row-start: 1;
+                  grid-row-end: 2;
+                  text-align: right;
+                  display:flex;
+                  justify-content:flex-end;
+                  align-items:flex-end;
+                  width: 30px;
+                }
+
+                .removeCell:hover, .thumbnailPlayCell:hover{
+                  color: red;
                 }
           `;
 
     // MY CODE
     const entity = this.config.entity;
-    let data = hass.states[entity].attributes.data
-    const json = typeof (data) == "object" ? hass.states[entity].attributes.data : JSON.parse(hass.states[entity].attributes.data);
+    let data = hass.states[entity].attributes.data;
+    const json =
+      typeof data == "object"
+        ? hass.states[entity].attributes.data
+        : JSON.parse(hass.states[entity].attributes.data);
     const max = json.length - 1;
+    const playerType = json[0]["player_type"].toLowerCase();
+    const kodi_entity_id = json[0]["kodi_entity_id"];
     //const max = Math.min(json.length - 1, this.config.max || 5);
 
-    this.content.innerHTML = "";
-    for (let count = 1; count <= max; count++) {
-      const item = key => json[count][key];
-      this.content.innerHTML += `
-        <div>
-          <ul class="playlist-items">
-            <li>
-              <div class="inner-item">
-                <div class="thumbnail">
-                  <div class="thumb_img" style="background-image: url('${item("thumbnail")}')"></div>
-                </div>
-                <div class="title">${item("label")}</div>
-                <div class="album">${item("album")}</div>
-                <div class="genre">${item("genre")}</div>
-                <div class="duration">${new Date(item("duration") * 1000).toISOString().substr(11, 8)}</div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        `;
-      if (count < max) {
-        this.content.innerHTML += `<br>`
+    this.content.innerHTML = ``;
+
+    // const playerEntity = json[0]["playerEntity"];
+
+    if (playerType == "video") {
+      this.content.innerHTML += `<div class="playerType"><ha-icon icon="mdi:movie"></ha-icon> Playlist</div>`;
+      for (let count = 1; count <= max; count++) {
+        const item = (key) => json[count][key];
+        this.content.innerHTML += `
+          <div class="inner-item">
+            <div class="thumbnailCell" style="background-image: url('${item(
+              "thumbnail"
+            )}')"> </div>
+            <div class="titleCell">${item("label")}</div>
+            <div class="genreCell">${
+              item("genre") ? item("genre") : "undefined"
+            }</div>
+            <ha-icon class="removeCell" position="${
+              count - 1
+            }" icon="mdi:delete"></ha-icon>
+          </div>
+          `;
+        if (count < max) {
+          this.content.innerHTML += `<br>`;
+        }
       }
+      this._bindButtons(this.card, this._hass, this.config, kodi_entity_id);
+    } else if (playerType == "audio") {
+      this.content.innerHTML += `<div class="playerType"><ha-icon icon="mdi:music"></ha-icon> Playlist</div>`;
+      for (let count = 1; count <= max; count++) {
+        const item = (key) => json[count][key];
+        this.content.innerHTML += `
+          <div class="inner-item">
+            <div class="thumbnailCell" style="background-image: url('${item(
+              "thumbnail"
+            )}')"> </div>
+            <ha-icon class="thumbnailPlayCell" icon="mdi:play" position="${
+              count - 1
+            }"></ha-icon>
+            <div class="titleCell">${item("label")}</div>
+            <div class="genreCell">${
+              item("genre") ? item("genre") : "undefined"
+            }</div>
+            <div class="albumCell">${item("album")}</div>
+            <div class="durationCell">${new Date(item("duration") * 1000)
+              .toISOString()
+              .substr(11, 8)}</div>
+            <ha-icon class="removeCell" position="${
+              count - 1
+            }" icon="mdi:delete"></ha-icon>
+          </div>
+          `;
+        if (count < max) {
+          this.content.innerHTML += `<br>`;
+        }
+      }
+      this._bindButtons(this.card, this._hass, this.config, kodi_entity_id);
+    }
+    this.appendChild(style);
+  }
+
+  _bindButtons(card, hass, config, kodi_entity_id) {
+    //GoTo buttons
+    var buttons = card.getElementsByClassName(`thumbnailPlayCell`);
+    var i;
+    for (i = 0; i < buttons.length; i++) {
+      let button = buttons[i];
+      button.addEventListener("click", function (source) {
+        const posn = parseInt(button.getAttribute("position"));
+        hass.callService("kodi", "call_method", {
+          entity_id: kodi_entity_id,
+          method: "Player.GoTo",
+          playerid: 0,
+          to: posn,
+        });
+        hass.callService("homeassistant", "update_entity", {
+          entity_id: config.entity,
+        });
+      });
     }
 
-    if (!this.querySelector('[id="umc_style"]')) this.appendChild(style);
+    // Remove buttons
+    var removeButtons = card.getElementsByClassName(`removeCell`);
+    var i;
+    for (i = 0; i < removeButtons.length; i++) {
+      let removeButton = removeButtons[i];
+      removeButton.addEventListener("click", function (source) {
+        const posn = parseInt(removeButton.getAttribute("position"));
+        // hass.callService("notify", "persistent_notification", { title: "hello", message: "removed "+kodi_entity_id });
+        hass.callService("kodi", "call_method", {
+          entity_id: kodi_entity_id,
+          method: "Playlist.Remove",
+          playlistid: 0,
+          position: posn,
+        });
+        hass.callService("homeassistant", "update_entity", {
+          entity_id: config.entity,
+        });
+      });
+    }
   }
-
-  next() {
-    //    this._hass.callService("notify", "persistent_notification", { message: "The garage door has been open for 10 minutes.", title: "Your Garage Door Friend" });
-    this._hass.callService("kodi", "call_method", { entity_id: this.entityId, method: "Player.GoTo", playerid: 0, to: "next" });
-  }
-
 
   setConfig(config) {
-    if (!config.service && !config.entity)
+    if (!config.service && !config.entity && !config.entityPlayer)
       throw new Error("Define entity.");
     this.config = config;
   }
